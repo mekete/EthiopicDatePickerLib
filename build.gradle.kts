b7104d8f7603214c7988d73b907b3e8636ec6d8a
makata <mail2teka@gmail.com>
2025-11-04 16:27:19 -0800

    mekete-restored-to-working-date-picker-with-distorted-year

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("com.android.library") version "8.3.0" apply false
+    id("com.android.library") version "8.12.3" apply false
 }
